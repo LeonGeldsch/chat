9ff7d7e6c3d225f5befb4cf9e047fd95944c3df6
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-03-03 13:11:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const socket = io('https://tuff-chat-app.herokuapp.com/:3000');
+const messageForm = document.querySelector('#send-container');
+const messageInput = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container');
+
+const name = prompt('What is your name?');
+appendMessage('You joined');
+socket.emit('new-user', name);
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`);
+});
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} connected`);
+});
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`);
+});
+
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    appendMessage(`You: ${message}`);
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+});
+
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
